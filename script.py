6656afcb16345c53cdbe3c3ad1d06bded36567e5
Anzola Fabio <anzola.fabio@gmail.com>
2021-11-20 16:42:19 +0100

    Method for yellow led

--- script.py
+++ script.py
@@ -2,15 +2,21 @@ import RPi.GPIO as GPIO
 import time
 
 LED_PIN_GR = 7
+LED_PIN_YE = 12
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(LED_PIN_GR, GPIO.OUT)
+GPIO.setup(LED_PIN_YE, GPIO.OUT)
 
 def toggleGreen():
     GPIO.output(LED_PIN_GR, GPIO.HIGH)
 
+def toggleYellow():
+    GPIO.output(LED_PIN_GR, GPIO.HIGH)
+
 try:
     toggleGreen()
+    toggleYellow()
     time.sleep(10)
 except KeyboardInterrupt:
     GPIO.cleanup()
