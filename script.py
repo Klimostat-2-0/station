4894acbc394df95c7cb1b20b2846e8cea1a21d36
Anzola Fabio <anzola.fabio@gmail.com>
2021-11-20 20:14:41 +0100

    co2 method

--- script.py
+++ script.py
@@ -1,14 +1,10 @@
 import RPi.GPIO as GPIO
+import mh_z19
 import time
-import adafruit_dht, board
 
 LED_PIN_GR = 7
 LED_PIN_YE = 12
-
-dht22gpiopin = 'D21'
-dhtboard = getattr(board, dht22gpiopin)
-
-dhtDevice = adafruit_dht.DHT22(board.D21, use_pulseio=False)
+CO2_PIN = 14
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(LED_PIN_GR, GPIO.OUT)
@@ -20,34 +16,14 @@ def toggleGreen():
 def toggleYellow():
     GPIO.output(LED_PIN_YE, GPIO.HIGH)
 
-def getSurroundings():
-    while True:
-        try:
-            # Print the values to the serial port
-            temperature_c = dhtDevice.temperature
-            temperature_f = temperature_c * (9 / 5) + 32
-            humidity = dhtDevice.humidity
-            print(
-                "Temp: {:.1f} F / {:.1f} C    Humidity: {}% ".format(
-                    temperature_f, temperature_c, humidity
-                )
-            )
+def getCO2():
+    mh_z19.read_from_pwm(CO2_PIN)
 
-        except RuntimeError as error:
-            # Errors happen fairly often, DHT's are hard to read, just keep going
-            print(error.args[0])
-            time.sleep(2.0)
-            continue
-        except Exception as error:
-            dhtDevice.exit()
-            raise error
-
-        time.sleep(2.0)
 
 try:
     toggleGreen()
     toggleYellow()
-    getSurroundings()
+    getCO2()
 except KeyboardInterrupt:
     GPIO.cleanup()
 
