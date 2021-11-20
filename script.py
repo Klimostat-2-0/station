3aa51625c847dd53e11baebe5e528f9a602448a2
Anzola Fabio <anzola.fabio@gmail.com>
2021-11-20 22:13:57 +0100

    temper sensor dht11

--- script.py
+++ script.py
@@ -1,17 +1,18 @@
 import RPi.GPIO as GPIO
 import mh_z19
 import time
-import board
-import adafruit_dht
+import Adafruit_DHT
+
 
 LED_PIN_GR = 7
 LED_PIN_YE = 12
 CO2_PIN = 14
+SENSE_PIN = 21
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(LED_PIN_GR, GPIO.OUT)
 GPIO.setup(LED_PIN_YE, GPIO.OUT)
-dhtDevice = adafruit_dht.DHT22(board.D21)
+DHTSensor = Adafruit_DHT.DHT11
 
 def toggleGreen():
     GPIO.output(LED_PIN_GR, GPIO.HIGH)
@@ -20,35 +21,16 @@ def toggleYellow():
     GPIO.output(LED_PIN_YE, GPIO.HIGH)
 
 def getCO2():
-    mh_z19.read_from_pwm(CO2_PIN)
+    return mh_z19.read_from_pwm(CO2_PIN)
 
 def getTempHum():
-    while temperature_c is None and humidity is None:
-        print("loop")
-        try:
-            # Print the values to the serial port
-            temperature_c = dhtDevice.temperature
-            temperature_f = temperature_c * (9 / 5) + 32
-            humidity = dhtDevice.humidity
-            print(
-                "Temp: {:.1f} F / {:.1f} C    Humidity: {}% ".format(
-                    temperature_f, temperature_c, humidity
-                )
-            )
-
-        except RuntimeError as error:
-            print(error.args[0])
-            time.sleep(2.0)
-            continue
-        except Exception as error:
-            dhtDevice.exit()
-            raise error
-        time.sleep(2.0)
-
+    _humid, _temper = Adafruit_DHT.read_retry(DHTSensor, SENSE_PIN)
+    return _humid, _temper
 try:
     toggleGreen()
     toggleYellow()
     getCO2()
+    getTempHum()
 except KeyboardInterrupt:
     GPIO.cleanup()
 
