e15fc26fc8576bce31186049f0790e17cb52be17
Anzola Fabio <anzola.fabio@gmail.com>
2021-11-20 17:05:46 +0100

    temp humid sensor

--- script.py
+++ script.py
@@ -1,11 +1,14 @@
 import RPi.GPIO as GPIO
-import Adafruit_DHT
 import time
+import adafruit_dht, board
 
 LED_PIN_GR = 7
 LED_PIN_YE = 12
-DHT_SENSOR = Adafruit_DHT.DHT22
-DHT_PIN = 21
+
+dht22gpiopin = 'D21'
+dhtboard = getattr(board, dht22gpiopin)
+
+dhtDevice = adafruit_dht.DHT22(dhtboard, use_pulseio=False)
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(LED_PIN_GR, GPIO.OUT)
@@ -18,17 +21,18 @@ def toggleYellow():
     GPIO.output(LED_PIN_YE, GPIO.HIGH)
 
 def getSurroundings():
-    humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
-    if humidity is not None and temperature is not None:
-        print("Temp={0:0.1f}*C  Humidity={1:0.1f}%".format(temperature, humidity))
-    else:
-        print("Failed to retrieve data from humidity sensor")
+    try:
+        temperature, humidity = dhtDevice.temperature, dhtDevice.humidity
+        print("Temperature: {:.1f} °C  Humidity: {:.1f} %".format(temperature, humidity))
+    except RuntimeError as error:
+        time.sleep(2.0)
+        temperature, humidity = dhtDevice.temperature, dhtDevice.humidity
+        print("Temperature: {:.1f} °C  Humidity: {:.1f} %".format(temperature, humidity))
 
 try:
     toggleGreen()
     toggleYellow()
     getSurroundings()
-    time.sleep(10)
 except KeyboardInterrupt:
     GPIO.cleanup()
 
