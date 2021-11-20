dc2b69899d8b8f9ca951d04eaa01f5fed700919f
Anzola Fabio <anzola.fabio@gmail.com>
2021-11-20 17:30:23 +0100

    h,t sensor

--- script.py
+++ script.py
@@ -8,7 +8,7 @@ LED_PIN_YE = 12
 dht22gpiopin = 'D21'
 dhtboard = getattr(board, dht22gpiopin)
 
-dhtDevice = adafruit_dht.DHT22(dhtboard, use_pulseio=False)
+dhtDevice = adafruit_dht.DHT22(board.D21, use_pulseio=False)
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(LED_PIN_GR, GPIO.OUT)
@@ -22,12 +22,23 @@ def toggleYellow():
 
 def getSurroundings():
     try:
-        temperature, humidity = dhtDevice.temperature, dhtDevice.humidity
-        print("Temperature: {:.1f} °C  Humidity: {:.1f} %".format(temperature, humidity))
+        # Print the values to the serial port
+        temperature_c = dhtDevice.temperature
+        temperature_f = temperature_c * (9 / 5) + 32
+        humidity = dhtDevice.humidity
+        print(
+            "Temp: {:.1f} F / {:.1f} C    Humidity: {}% ".format(
+                temperature_f, temperature_c, humidity
+            )
+        )
+
     except RuntimeError as error:
+        # Errors happen fairly often, DHT's are hard to read, just keep going
+        print(error.args[0])
         time.sleep(2.0)
-        temperature, humidity = dhtDevice.temperature, dhtDevice.humidity
-        print("Temperature: {:.1f} °C  Humidity: {:.1f} %".format(temperature, humidity))
+    except Exception as error:
+        dhtDevice.exit()
+        raise error
 
 try:
     toggleGreen()
